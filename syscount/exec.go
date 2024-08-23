7ff4fad711c6d97e4aaf94eada57940d20833ec2
InnaSaul <66169023+InnaSaul@users.noreply.github.com>
2024-08-23 13:19:32 +0300

    Update exec.go
    change of function's scope

--- syscount/exec.go
+++ syscount/exec.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-func metricTime(start time.Time) {
+func MetricTime(start time.Time) {
 	// функция Now() возвращает текущее время, а функция Sub возвращает разницу между двумя временными метками
 	fmt.Println(time.Now().Sub(start))
 }
