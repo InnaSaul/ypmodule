39af75ce33374ad60f45e99fe8050e8a8fcd9722
InnaSaul <innsaulin@mail.ru>
2024-08-23 13:06:08 +0300

    Cleaning of new package syscount

--- syscount/exec.go
+++ syscount/exec.go
@@ -9,11 +9,3 @@ func metricTime(start time.Time) {
 	// функция Now() возвращает текущее время, а функция Sub возвращает разницу между двумя временными метками
 	fmt.Println(time.Now().Sub(start))
 }
-
-/*
-func VeryLongTimeFunction () {
-	// передаём в функцию metricTime значение текущего времени и откладываем её вызов до возврата
-        defer     metricTime(time.Now())
-		// Какие-то долгие вычисления
-    }
-*/
